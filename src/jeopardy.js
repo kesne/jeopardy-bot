9bb535b4687bcad93af5686537a66ae2f9570932
Jordan Gensler <jordan.gensler@nike.com>
2015-09-30 00:19:54 -0700

    More scaffolding

--- src/jeopardy.js
+++ src/jeopardy.js
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
+import express from 'express';
+import { Game } from 'models/Game';
+import { Person } from 'models/Person';
 
 mongoose.connect('mongodb://localhost/test');
 
-var Cat = mongoose.model('Cat', { name: String });
+const app = express();
 
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) // ...
-  console.log('meow');
+// TODO: Body parsing
+// TODO: Stateless, so we can resume back into an in-progress game.
+
+app.post('/command', (req, res) => {
+
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Jeopardy Bot listening on port ${port}`);
 });
