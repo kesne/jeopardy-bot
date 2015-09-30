5e2f012bdac1567cefc4b7d1ed317424bd0889b4
Jordan Gensler <jordan.gensler@nike.com>
2015-09-30 02:58:09 -0700

    Board now works based on the mongo db.

--- src/jeopardy.js
+++ src/jeopardy.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import express from 'express';
+import { join } from 'path';
+import { dust } from 'adaro';
 import { Game } from './models/Game';
 import { Person } from './models/Person';
 
@@ -7,11 +9,74 @@ mongoose.connect('mongodb://localhost/test');
 
 const app = express();
 
+const options = {
+  helpers: [
+    (dust) => {
+      dust.helpers.Card = (chunk, context, bodies, params) => {
+        const questions = context.get('questions');
+        const value = context.resolve(params.value);
+        const id = context.resolve(params.id);
+        var question = questions.find((q) => {
+          return q.value === value && q.category_id === id;
+        });
+        if (question.answered) {
+          chunk.write('');
+        } else {
+          chunk.write('$' + value);
+        }
+      }
+    }
+  ]
+}
+
+app.engine('dust', dust(options));
+app.set('view engine', 'dust');
+app.set('views', join(__dirname, 'views'));
+
 // TODO: Body parsing
 // TODO: Stateless, so we can resume back into an in-progress game.
 
 app.post('/command', (req, res) => {
 
+});
+
+app.get('/answer', (req, res) => {
+  Game.activeGame().then(game => {
+    const id = parseInt(req.query.id, 10);
+    game.questions.some((q) => {
+      if (q.id === id) {
+        q.answered = true;
+        return true;
+      }
+    });
+    return game.save();
+  }).then(() => {
+    res.send('done');
+  });
+});
+
+app.get('/', (req, res) => {
+  Game.activeGame().then(game => {
+    res.render('board', {
+      categories: game.categories,
+      questions: game.questions,
+      values: [200, 400, 600, 800, 1000]
+    });
+  });
+});
+
+app.get('/startgame', (req, res) => {
+  Game.start().then(() => {
+    res.send('ok');
+  }).catch((e) => {
+    res.send('no');
+  });
+});
+
+app.get('/endgame', (req, res) => {
+  Game.end().then(() => {
+    res.send('ok');
+  });
 });
 
 const port = process.env.PORT || 8000;
--- src/models/Game.js
+++ src/models/Game.js
@@ -1,9 +1,74 @@
+import fetch from 'node-fetch';
 import {Schema, model} from 'mongoose';
+
+const PAGE_LENGTH = 6;
+const LAST_PAGE = 18412;
+
+const jServiceCategories = () => {
+  const randomPage = Math.ceil(Math.random() * LAST_PAGE);
+  return fetch(`http://jservice.io/api/categories?count=${PAGE_LENGTH}&offset=${randomPage}`)
+    .then(res => res.json())
+    .then(categories => {
+      // Invalid category set for some reason. Try again.
+      if (categories.length !== 6) {
+        return jServiceCategories();
+      }
+      return categories;
+    });
+};
+
+const jServiceCategory = (id) => {
+  return fetch(`http://jservice.io/api/category?id=${id}`)
+    .then(res => res.json())
+    .then(res => {
+      let clues = [];
+      let reclaimed = [];
+      let found = 0;
+      res.clues.some(clue => {
+        if (clue.value) {
+          if (!clues[clue.value]) {
+            clues[clue.value] = clue;
+            found++;
+          }
+        } else {
+          // This question wasn't included because it doesn't have a value,
+          // but we could assign one in to "reclaim" this question:
+          reclaimed.push(clue);
+        }
+        return found === 5;
+      });
+
+      // Bad category set, let's reclaim it!
+      if (found < 5) {
+        [200, 400, 600, 800, 1000].forEach((value) => {
+          if (!clues[value]) {
+            clues[value] = reclaimed.pop();
+            // Assign it the value we gave it:
+            clues[value].value = value;
+          }
+        });
+      }
+
+      // Return the clues:
+      return clues.filter(c => c);
+    });
+}
+
+async function jServiceQuestions(ids) {
+  let questions = await Promise.all(
+    ids.map(id => jServiceCategory(id))
+  );
+  return [].concat(...questions);
+};
 
 export const schema = new Schema({
   categories: [{
     id: {
       type: Number,
+      required: true
+    },
+    title: {
+      type: String,
       required: true
     }
   }],
@@ -12,8 +77,72 @@ export const schema = new Schema({
     id: {
       type: Number,
       required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    },
+    question: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Number,
+      required: true
+    },
+    answered: {
+      type: Boolean,
+      default: false
+    },
+    category_id: {
+      type: Number,
+      required: true
     }
   }]
 });
 
-export const Person = model('Game', schema);
+// Grab the active game:
+schema.statics.activeGame = function() {
+  return this.findOne({'questions': {$elemMatch: {answered: false}}});
+};
+
+// End all games:
+schema.statics.end = function() {
+  return this.remove();
+};
+
+// Start a new game:
+schema.statics.start = async function() {
+  const active = await this.activeGame();
+  if (active) {
+    throw new Error('An active game is already in progress.');
+  }
+  await Game.end();
+  const categories = await jServiceCategories();
+  const questions = await jServiceQuestions(categories.map(cat => cat.id));
+  return this.create({
+    categories,
+    questions
+  });
+
+
+  // return this.activeGame()
+  //   .then(active => {
+  //     if (active)
+  //       throw new Error('An active game is already in progress.');
+  //   })
+  //   .then(() => Game.end())
+  //   .then(() => jServiceCategories())
+  //   .then(categories => Promise.all([
+  //     categories,
+  //     ...categories.map(cat => jServiceQuestions(cat.id))
+  //   ]))
+  //   .then(([categories, ...questions]) => 
+  //     this.create({
+  //       categories,
+  //       questions: [].concat(...questions)
+  //     })
+  //   );
+}
+
+export const Game = model('Game', schema);
