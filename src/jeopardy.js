35955690392b8c11f2f9c5d8caea7469384c2049
Jordan Gensler <jordan.gensler@nike.com>
2015-10-04 12:35:27 -0700

    Print the board back when a question is answered.

--- src/jeopardy.js
+++ src/jeopardy.js
@@ -40,15 +40,16 @@ export const commands = {
     }
     if (correct) {
       const game = await Game.activeGame();
-      console.log(game.activeClue.value, person);
       // Award the value:
       await person.correct(game.activeClue.value);
+      // Get the new board url:
+      const url = getImageUrl('board');
       // Mark the question as answered:
       await Game.answer();
-      return `That is correct, ${person.name}. Your score is $${person.score}.`;
+      return `That is correct, ${person.name}. Your score is $${person.score}. Select a new category. ${url}`;
     } else {
       await person.incorrect(game.activeClue.value);
-      return `That is incorrect, ${person.name}. Your score is now $${person.score}.`;
+      return `That is incorrect, ${person.name}. Your score is now $${person.score}. ${url}`;
     }
   },
 
