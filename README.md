a1c5fd982af716220665bb38304e8140c0cad201
Jordan Gensler <jordangens@gmail.com>
2015-10-05 19:37:29 -0700

    Working on some stuff for heroku once we get this thing off the ground.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Jeopardy Bot
 
 A cool slack jeopardy bot.
+
+## Configuring
+
+You can configure the bot via environment variables.
+All of the configuration options (with one exception) are prefixed with `JBOT_` to prevent conflicts.
+
+- JBOT_MODE
+- JBOT_ROOMS
+- JBOT_VERIFY_TOKENS
+- JBOT_API
+- JBOT_USERNAME
+- MONGOLAB_URI
--- src/config.js
+++ src/config.js
@@ -1,8 +1,21 @@
+/*
+ * BOT CONFIGURATION:
+ */
 // The mode the bot is in:
-export const MODE = 'response';
+export const MODE = process.env.JBOT_MODE || 'response';
+
+// A list of rooms that the bot will work in:
+export let ROOM_WHITELIST = process.env.JBOT_ROOMS || 'jeopardy';
+ROOM_WHITELIST = ROOM_WHITELIST.split(',');
+
+// The outgoing webhook token used to verify requests:
+export let VERIFY_TOKENS = process.env.JBOT_VERIFY_TOKENS || '';
+VERIFY_TOKENS = VERIFY_TOKENS.split(',');
+
+export const API_KEY = process.env.JBOT_API || '';
 
 // The username of the bot:
-export const USERNAME = 'JeopardyBot';
+export const USERNAME = process.env.JBOT_USERNAME || 'JeopardyBot';
 // The ID of the bot that we can use to ignore messages from ourself:
 export const BOT_ID = 'USLACKBOT';
 
@@ -11,6 +24,10 @@ export const MONGO = process.env.MONGOLAB_URI || 'mongodb://localhost/jeopardy'
 
 // The port for the application:
 export const PORT = process.env.PORT || 8000;
+
+/*
+ * GAME CONFIGURATION
+ */
 
 // The question values we want:
 export const VALUES = [
@@ -26,6 +43,9 @@ export const LAST_PAGE = 18412;
 
 // The number of seconds to wait before a clue is timed out.
 export const CLUE_TIMEOUT = 45;
+
+// The number of seconds to give control of the board to the correct guesser.
+export const BOARD_CONTROL_TIMEOUT = 10;
 
 // How many things we want:
 export const CATEGORY_COUNT = 6;
