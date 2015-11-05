5be23404421dedaa557f3c9ae65d83ab5f5768ff
Jordan Gensler <jordan.gensler@airbnb.com>
2015-11-05 14:15:51 -0800

    Failing auto challenges if we don't have an API key

--- src/commands/autochallenge.js
+++ src/commands/autochallenge.js
@@ -5,6 +5,10 @@ const cx = '013673686761662547163:nw_cf3t8esg';
 const URL_BASE = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${cx}`;
 
 export default async function autoChallenge(answer, guess) {
+  // We can only auto challenge if we have an API key:
+  if (!GOOGLE_API_KEY) {
+    return false;
+  }
   const [{items: [answerResult]}, {items: [guessResult]}] = await Promise.all([
     fetch(`${URL_BASE}&q=${answer}`),
     fetch(`${URL_BASE}&q=${guess}`)
