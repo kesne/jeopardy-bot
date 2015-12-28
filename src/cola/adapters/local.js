4e92c09dc8e62b916907a850a9d00d9d6466db45
Jordan Gensler <jordan.gensler@airbnb.com>
2015-12-28 03:46:40 -0800

    Commenting out code for babel

--- src/cola/adapters/local.js
+++ src/cola/adapters/local.js
@@ -1,36 +1,36 @@
-/**
- * ADAPTER CONFIGURATION
- */
-
-export const CAPTURE_ALL_CLUES = true;
-
-/**
- * ADAPTER API
- */
-
-export async function setup() {
-
-}
-
-export async function board() {
-
-}
-
-export async function clue() {
-
-}
-
-
-function saveImage(filename, image) {
-  return new Promise((resolve, reject) => {
-    image.saveAsync(filename, err => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(filename);
-    });
-  });
-}
-
-
-  return await saveImage(join(temporaryDirectory, `fullboard.${game.channel_id}.png`), board);
+// /**
+//  * ADAPTER CONFIGURATION
+//  */
+//
+// export const CAPTURE_ALL_CLUES = true;
+//
+// /**
+//  * ADAPTER API
+//  */
+//
+// export async function setup() {
+//
+// }
+//
+// export async function board() {
+//
+// }
+//
+// export async function clue() {
+//
+// }
+//
+//
+// function saveImage(filename, image) {
+//   return new Promise((resolve, reject) => {
+//     image.saveAsync(filename, err => {
+//       if (err) {
+//         return reject(err)
+//       }
+//       resolve(filename);
+//     });
+//   });
+// }
+//
+//
+//   return await saveImage(join(temporaryDirectory, `fullboard.${game.channel_id}.png`), board);
